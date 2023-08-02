7d104a7884eecf91b0b5f2bc94ae30fcedcdab9c
thanhvt <pysga1996@gmail.com>
2023-08-02 17:48:50 +0700

    change db from postgresql to mariadb

--- src/main/java/com/alpha/config/general/CommonConfig.java
+++ src/main/java/com/alpha/config/general/CommonConfig.java
@@ -5,34 +5,24 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.StorageClient;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.time.Duration;
-import java.util.List;
-import java.util.Objects;
 import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
-import org.apache.catalina.Context;
-import org.apache.catalina.connector.Connector;
-import org.apache.coyote.http2.Http2Protocol;
-import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnCloudPlatform;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.cloud.CloudPlatform;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.embedded.tomcat.TomcatWebServer;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.List;
+import java.util.Objects;
 
 @Log4j2
 @Configuration
@@ -46,18 +36,6 @@ public class CommonConfig {
 
     @Value("${storage.firebase.storage-bucket}")
     private String firebaseStorageBucket;
-
-    @Value("${custom.http-port}")
-    private Integer httpPort;
-
-    @Value("${custom.https-port}")
-    private Integer httpsPort;
-
-    @Value("${custom.security-policy}")
-    private String securityPolicy;
-
-    @Value("${custom.connector-scheme}")
-    private String connectorScheme;
 
     @Value("${storage.firebase.credentials}")
     private String firebaseCredentials;
@@ -99,35 +77,6 @@ public class CommonConfig {
             log.error("Could not get admin-sdk json file. Please try again!", ex);
             throw ex;
         }
-    }
-
-    @Bean
-    @Profile("default")
-    public ServletWebServerFactory servletContainer() {
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
-            @Override
-            protected void postProcessContext(Context context) {
-                SecurityConstraint securityConstraint = new SecurityConstraint();
-                // set to CONFIDENTIAL to automatically redirect from http to https port
-                securityConstraint.setUserConstraint(securityPolicy);
-                SecurityCollection collection = new SecurityCollection();
-                collection.addPattern("/*");
-                securityConstraint.addCollection(collection);
-                context.addConstraint(securityConstraint);
-            }
-        };
-        tomcat.addAdditionalTomcatConnectors(getHttpConnector());
-        return tomcat;
-    }
-
-    private Connector getHttpConnector() {
-        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
-        connector.setScheme(connectorScheme);
-        connector.setPort(httpPort);
-        connector.setSecure(false);
-        connector.setRedirectPort(httpsPort);
-        connector.addUpgradeProtocol(new Http2Protocol());
-        return connector;
     }
 
     @Bean
